aba8d4c8f15234a48e3759ac13040c7473ab945d
Saisree <saisreevaddi@gmail.com>
2023-01-20 00:16:37 +0530

    Initial Commit

---
+++ src/test/java/com/exam/ExamPortalSBandAnglr/ExamPortalSBandAnglrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.ExamPortalSBandAnglr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPortalSBandAnglrProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
